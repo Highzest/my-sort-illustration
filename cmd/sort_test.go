7e103433e7f4b71496b2079f307cd016e9b2b89f
Highzest <epmek96@gmail.com>
2021-10-31 12:31:11 +0600

    feature: Merge Sort is implemented

--- cmd/sort_test.go
+++ cmd/sort_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestBubbleSort(t *testing.T) {
-	t.Run("test bubble sort", func(t *testing.T) {
+	t.Run("simple case", func(t *testing.T) {
 		snapshots := [][]int{
 			{5, 1, 4, 2, 6, 9, 10, 2}, // initial array
 			{1, 5, 4, 2, 6, 9, 10, 2}, // i = 0, j = 1, switch
@@ -25,7 +25,7 @@ func TestBubbleSort(t *testing.T) {
 
 		ap := algoProcess{
 			current:  current,
-			stepSort: bubbleSort(),
+			stepSort: bubbleSort(snapshots[0]),
 		}
 		i := 1
 
@@ -36,11 +36,10 @@ func TestBubbleSort(t *testing.T) {
 			i++
 		}
 	})
-
 }
 
 func TestHeapSort(t *testing.T) {
-	t.Run("test heap sort", func(t *testing.T) {
+	t.Run("simple case", func(t *testing.T) {
 		snapshots := [][]int{
 			{3, 5, 1, 4, 7, 6, 2, 8}, // initial array
 			{3, 5, 1, 8, 7, 6, 2, 4}, // i = 3, heapify start
@@ -61,7 +60,38 @@ func TestHeapSort(t *testing.T) {
 
 		ap := algoProcess{
 			current:  current,
-			stepSort: heapSort(),
+			stepSort: heapSort(snapshots[0]),
+		}
+		i := 1
+
+		for !ap.done {
+			if guessed := ap.next(snapshots[i]); !guessed {
+				t.Fatalf("failed at step %d, the snapshot was %v", i, snapshots[i])
+			}
+			i++
+		}
+	})
+}
+
+func TestMergeSort(t *testing.T) {
+	t.Run("simple case", func(t *testing.T) {
+		snapshots := [][]int{
+			{5, 3, 4, 1, 7, 6, 8, 2}, // initial array
+			{3, 5, 4, 1, 7, 6, 8, 2}, // 3, 5
+			{3, 5, 1, 4, 7, 6, 8, 2}, // 1, 4
+			{1, 3, 4, 5, 7, 6, 8, 2}, // 1, 3, 4, 5
+			{1, 3, 4, 5, 6, 7, 8, 2}, // 6, 7
+			{1, 3, 4, 5, 6, 7, 2, 8}, // 2, 8
+			{1, 3, 4, 5, 2, 6, 7, 8}, // 2, 6, 7, 8
+			{1, 2, 3, 4, 5, 6, 7, 8}, // end
+		}
+
+		current := make([]int, len(snapshots[0]))
+		copy(current, snapshots[0])
+
+		ap := algoProcess{
+			current:  current,
+			stepSort: mergeSort(snapshots[0]),
 		}
 		i := 1
 
@@ -73,4 +103,34 @@ func TestHeapSort(t *testing.T) {
 		}
 	})
 
+	t.Run("complex case", func(t *testing.T) {
+		snapshots := [][]int{
+			{5, 3, 4, 1, 7, 6, 8, 2, 14, 12, 13, 10, 11, 9}, // initial array
+			{3, 4, 5, 1, 7, 6, 8, 2, 14, 12, 13, 10, 11, 9}, // 3, 4, 5
+			{3, 4, 5, 1, 6, 7, 8, 2, 14, 12, 13, 10, 11, 9}, // 1, 6, 7, 8
+			{1, 3, 4, 5, 6, 7, 8, 2, 14, 12, 13, 10, 11, 9}, // 1, 3, 4, 5, 6, 7, 8
+			{1, 3, 4, 5, 6, 7, 8, 2, 12, 14, 13, 10, 11, 9}, // 12, 14
+			{1, 3, 4, 5, 6, 7, 8, 2, 12, 14, 10, 13, 11, 9}, // 10, 13
+			{1, 3, 4, 5, 6, 7, 8, 2, 12, 14, 10, 13, 9, 11}, // 9, 11
+			{1, 3, 4, 5, 6, 7, 8, 2, 12, 14, 9, 10, 11, 13}, // 9, 10, 11, 13
+			{1, 3, 4, 5, 6, 7, 8, 2, 9, 10, 11, 12, 13, 14}, // 2, 9, 10, 11, 12, 13, 14
+			{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}, // end
+		}
+
+		current := make([]int, len(snapshots[0]))
+		copy(current, snapshots[0])
+
+		ap := algoProcess{
+			current:  current,
+			stepSort: mergeSort(snapshots[0]),
+		}
+		i := 1
+
+		for !ap.done {
+			if guessed := ap.next(snapshots[i]); !guessed {
+				t.Fatalf("failed at step %d, the snapshot was %v", i, snapshots[i])
+			}
+			i++
+		}
+	})
 }
